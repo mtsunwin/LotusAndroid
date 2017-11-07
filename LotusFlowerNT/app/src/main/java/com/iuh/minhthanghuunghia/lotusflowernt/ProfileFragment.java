4a33394d606ec31b51db033e3f93345d9012ba1c
Tran Thang <mt.tranthang@gmail.com>
2017-11-07 19:22:30 +0700

    Fragment

---
+++ LotusFlowerNT/app/src/main/java/com/iuh/minhthanghuunghia/lotusflowernt/ProfileFragment.java
@@ -0,0 +1,26 @@
+package com.iuh.minhthanghuunghia.lotusflowernt;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iuh.minhthanghuunghia.lotusflowernt.databinding.ActivityProfileFragmentBinding;
+
+/**
+ * Created by ThinkPad on 11/7/2017.
+ */
+
+public class ProfileFragment extends Fragment {
+    private ActivityProfileFragmentBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.activity_profile_fragment, container, false);
+        return binding.getRoot();
+    }
+}
