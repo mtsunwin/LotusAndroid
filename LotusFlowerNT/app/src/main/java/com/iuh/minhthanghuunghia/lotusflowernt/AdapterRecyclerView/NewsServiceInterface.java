77d75e1d97ed15baedf8eb06c37fe8aa1c921458
Tran Thang <mt.tranthang@gmail.com>
2017-11-14 13:39:24 +0700

    DELETE ALL

--- LotusFlowerNT/app/src/main/java/com/iuh/minhthanghuunghia/lotusflowernt/AdapterRecyclerView/NewsServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.iuh.minhthanghuunghia.lotusflowernt.AdapterRecyclerView;
-
-/**
- * Created by ThinkPad on 11/12/2017.
- */
-
-public interface NewsServiceInterface {
-    void updateData();
-}
